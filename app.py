8c3c53e5be81f8a72496b3ad4a76a186cf0aa2b9
zpc <zip_city@163.com>
2023-12-23 19:30:29 +0800

    inject user into templates

--- app.py
+++ app.py
@@ -61,16 +61,19 @@ def forge():
     db.session.commit()
     click.echo('Done.')
 
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
+
 @app.route("/")
 def index():
-    user = User.query.first()
     movies = Movie.query.all()
-    return render_template('index.html', user=user, movies=movies)
+    return render_template('index.html', movies=movies)
 
 @app.errorhandler(404)
 def page_not_found(e):
-    user = User.query.first()
-    return render_template('404.html', user=user), 404
+    return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
